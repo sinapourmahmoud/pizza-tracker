82f7edfccefd120189a5e338e0e926af5ea98afb
sinapourmahmoud <sinapoormahmood@gmail.com>
2026-09-05 16:24:44 -0500

    made the initial command for db

---
+++ internal/models/models.go
@@ -0,0 +1,38 @@
+package models
+
+
+import (
+	"fmt"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+
+type DBModel struct {
+	Order OrderModel
+}
+
+
+func InitDB(dataSourceName string) (*DBModel,error) {
+	db,err :=gorm.Open(sqlite.Open(dataSourceName),&gorm.Config{})
+
+	if err !=nil{
+		return nil,fmt.Errorf("Failed to migrate database: %v",err)
+
+	}
+
+	err = db.AutoMigrate(&Order{},&OrderItem{})
+
+	if err !=nil{
+		return nil, fmt.Errorf("Failed to migrate database %v",err)
+	}
+
+	dbModel := &DBModel{
+		Order : OrderModel{DB:db},
+	}
+	return dbModel,nil
+
+
+
+}
--- internal/models/order.go
+++ internal/models/order.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"time"
+
 	"github.com/teris-io/shortid"
 	"gorm.io/gorm"
 )
@@ -32,46 +33,44 @@ type OrderModel struct {
 }
 
 type Order struct {
-	ID string `gorm:"primaryKey; size:14" json:"id"`
-	Status string `gorm:"not null" json:"status"`
-	CustomerName string `gorm:"not null" json:"customerName"`
-	Phone string `gorm:"not null" json:"phone"`
-	Address string `gorm:"not null" json:"address"`
-	Items []OrderItem `gorm:"foreignkey:OrderID" json:"pizzas"`
-	CreatedAt time.Time `json:"createdAt"`
+	ID           string      `gorm:"primaryKey; size:14" json:"id"`
+	Status       string      `gorm:"not null" json:"status"`
+	CustomerName string      `gorm:"not null" json:"customerName"`
+	Phone        string      `gorm:"not null" json:"phone"`
+	Address      string      `gorm:"not null" json:"address"`
+	Items        []OrderItem `gorm:"foreignkey:OrderID" json:"pizzas"`
+	CreatedAt    time.Time   `json:"createdAt"`
 }
 
 type OrderItem struct {
-	ID string `gorm:"primaryKey; size:14" json:"id"`
-	OrderID string `gorm:index;`
-	Size string `gorm:"not null" json:"size"`
-	Pizza string `gorm:"not null" json:"pizza"`
+	ID           string `gorm:"primaryKey; size:14" json:"id"`
+	OrderID      string `gorm:index;`
+	Size         string `gorm:"not null" json:"size"`
+	Pizza        string `gorm:"not null" json:"pizza"`
 	Instructions string `json:"instructions"`
-
 }
 
 func (o *Order) BeforeCreate(tx *gorm.DB) error {
-	if o.ID == ""{
+	if o.ID == "" {
 		o.ID = shortid.MustGenerate()
 	}
 	return nil
 }
 
-
 func (oi *OrderItem) BeforeCreate(tx *gorm.DB) error {
-	if oi.ID == ""{
+	if oi.ID == "" {
 		oi.ID = shortid.MustGenerate()
 	}
 	return nil
 }
 
-func (o *OrderModel) CreateOrder(order *Order) error{
+func (o *OrderModel) CreateOrder(order *Order) error {
 	return o.DB.Create(order).Error
 }
-func (o *OrderModel) GetOrder(id string) (*Order,error){
+func (o *OrderModel) GetOrder(id string) (*Order, error) {
 	var order Order
-	err := o.DB.Preload("Items").First(&order,"id = ?",id).Error
+	err := o.DB.Preload("Items").First(&order, "id = ?", id).Error
 
-	return &order,err
-	
+	return &order, err
+
 }
