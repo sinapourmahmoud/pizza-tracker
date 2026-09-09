5bb41206cc4af16d5105448a5e34e0ed93b1441e
sinapourmahmoud <sinapoormahmood@gmail.com>
2026-09-09 11:24:44 +0330

    handle main file

---
+++ cmd/main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"log/slog"
+	"os"
+	"pizza-tracker/internal/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+
+	cfg := loadConfig()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	slog.SetDefault(logger)
+
+	dbModel, err := models.InitDB(cfg.DBPath)
+
+	if err != nil {
+
+		slog.Error("Failed to initialize database", "error", err)
+
+		os.Exit(1)
+		slog.Info("Database initialized successfuly")
+
+	}
+
+	RegisterCustomValidators()
+
+	h := NewHandler(dbModel)
+
+	router := gin.Default()
+
+	if err := loadTemplates(router); err != nil {
+		slog.Error("Failed to load templates")
+		os.Exit(1)
+	}
+
+	setupRoutes(router,h)
+
+	slog.Info("Server starting","url","http://localhost"+cfg.Port)
+
+	router.Run(":"+cfg.Port)
+
+}
